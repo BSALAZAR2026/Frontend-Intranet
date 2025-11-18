ec35485a8dcefff695062a7839fb472483ce2a78
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-11-18 10:51:12 -0500

    Update login

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginCredentials } from '../../pages/login/login';
+import { API_BASE_URL, API_ENDPOINTS } from '../constants/api.constants';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  private apiUrl = 'http://localhost:8080/api/auth';
+  private apiUrl = `${API_ENDPOINTS.auth}`;
 
   login(credentials: LoginCredentials): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, credentials);
