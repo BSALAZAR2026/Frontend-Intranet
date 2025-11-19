63c576addc9ff366bb9b8c99473572a922a05ee9
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-11-19 09:45:56 -0500

    fix: update api-gateway url

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://intranet-gateway.onrender.com/api',
+  apiGatewayUrl: 'https://intranet-api-gateway.onrender.com',
   eurekaUrl: 'https://intranet-eureka-server.onrender.com/eureka'
 };
