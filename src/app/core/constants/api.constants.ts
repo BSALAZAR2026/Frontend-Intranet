95ab391f3375270042570e6be10d71f1ab031257
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-11-19 09:47:16 -0500

    fix: update urls

--- src/app/core/constants/api.constants.ts
+++ src/app/core/constants/api.constants.ts
@@ -3,8 +3,8 @@ import { environment } from '../../../enviroments/enviroment.prod';
 export const API_BASE_URL = environment.apiGatewayUrl;
 
 export const API_ENDPOINTS = {
-  auth: `${API_BASE_URL}/auth`,
-  users: `${API_BASE_URL}/users`,
-  documents: `${API_BASE_URL}/documents`,
-  birthdays: `${API_BASE_URL}/users/birthdays`
+  auth: `${API_BASE_URL}/api/auth`,
+  users: `${API_BASE_URL}/api/users`,
+  documents: `${API_BASE_URL}/api/documents`,
+  birthdays: `${API_BASE_URL}/api/users/birthdays`
 };
