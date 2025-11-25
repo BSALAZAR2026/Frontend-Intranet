0b55b143db3d51210b6dd7502f493fa588ddcefa
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-11-25 15:05:52 -0500

    Loading fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GlobalSpinnerComponent } from "./shared/components/global-spinner/global-spinner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GlobalSpinnerComponent],
   standalone: true,
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
