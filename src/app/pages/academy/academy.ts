35273e3de78106df2314dfb7df6801468a8ea5de
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-12-04 14:21:04 -0500

    Update academy

--- src/app/pages/academy/academy.ts
+++ src/app/pages/academy/academy.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VIDEOS } from './videos.data';
+import { SafeUrlPipe } from "./safe-url.pipe";
 
 @Component({
   selector: 'app-academia',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, SafeUrlPipe],
   templateUrl: './academy.html',
   styleUrl: './academy.scss'
 })
-export class AcademyComponent {
+export class AcademyComponent implements OnInit, AfterViewInit {
 
+  allVideos = VIDEOS;
+  loadedVideos: string[] = [];
+  itemsPerLoad = 3;
+
+  @ViewChild('infiniteTrigger') infiniteTrigger!: ElementRef;
+
+  ngOnInit() {
+    this.loadMore();
+  }
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(entries => {
+      if (entries.some(e => e.isIntersecting)) {
+        this.loadMore();
+      }
+    });
+
+    observer.observe(this.infiniteTrigger.nativeElement);
+  }
+
+  loadMore() {
+    const next = this.allVideos.splice(0, this.itemsPerLoad);
+    this.loadedVideos.push(...next);
+  }
 }
---
+++ src/app/pages/academy/videos.data.ts
@@ -0,0 +1,7 @@
+export const VIDEOS = [
+ "https://www.youtube.com/embed/JXc9iQXRMLg?autoplay=0&mute=1",
+  "https://prosciencelab-my.sharepoint.com/...UniqueId=5b5e3df7",
+  "https://prosciencelab-my.sharepoint.com/...UniqueId=ea9a08d3",
+  "https://prosciencelab-my-sharepoint.com/...UniqueId=dc4f8ae1",
+];
+
