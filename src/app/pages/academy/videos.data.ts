3e72140b9bc3ecb9fe48ce556be2885e78ce469b
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-12-05 09:52:00 -0500

    add videos Dr Wilson

--- src/app/pages/academy/videos.data.ts
+++ src/app/pages/academy/videos.data.ts
@@ -1,7 +1,6 @@
 export const VIDEOS = [
- "https://www.youtube.com/embed/JXc9iQXRMLg?autoplay=0&mute=1",
-  "https://prosciencelab-my.sharepoint.com/...UniqueId=5b5e3df7",
-  "https://prosciencelab-my.sharepoint.com/...UniqueId=ea9a08d3",
-  "https://prosciencelab-my-sharepoint.com/...UniqueId=dc4f8ae1",
+  "https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=dc4f8ae1-13da-43bd-ad13-60fe2c337380&embed=%7B%22ust%22%3Afalse%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create",
+  "https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=ea9a08d3-cd51-4fad-bfc8-abd4dfcf6f6a&embed=%7B%22ust%22%3Afalse%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create",
+  "https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=5b5e3df7-c202-4654-9f96-7d68a9361fc5&embed=%7B%22ust%22%3Afalse%2C%22hv%22%3A%22CopyEmbedCode%22%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create",
 ];
 
