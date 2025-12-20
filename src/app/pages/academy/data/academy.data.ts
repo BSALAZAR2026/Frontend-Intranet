cdab251f04e8830a08442c0dfa5de541ddb51172
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-12-20 13:07:43 -0500

    Academy V 1.8.4

--- src/app/pages/academy/data/academy.data.ts
+++ src/app/pages/academy/data/academy.data.ts
@@ -6,7 +6,7 @@ export const COURSES: Course[] = [
     title: 'Inducción ProScience',
     description: 'Curso de bienvenida y lineamientos generales',
     videoUrl: 'https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=dc4f8ae1-13da-43bd-ad13-60fe2c337380',
-    examUrl: 'https://forms.office.com/r/XXXXX',
+    examUrl: 'https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&id=wuhu0UwHzUWb_hhHjC1yyXs2qqn3lLBAo1y3ITkn_01UMVUzTFUzM0ozODNTQzFLQzY1N05IQk1VRC4u',
     videoCompleted: false,
     examPassed: false,
     duration: 0
