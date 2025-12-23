eeb673df691fef4ec1a9e274b3576f839ff9b944
LuisVelez1 <126474251+LuisVelez1@users.noreply.github.com>
2025-12-23 10:30:42 -0500

    Academy V 1.8.9

--- src/app/pages/academy/data/academy.data.ts
+++ src/app/pages/academy/data/academy.data.ts
@@ -10,5 +10,25 @@ export const COURSES: Course[] = [
     videoCompleted: false,
     examPassed: false,
     duration: 0
-  }
+  },
+  {
+    id: 2,
+    title: 'Inducción ProScience',
+    description: 'Curso de proteínas',
+    videoUrl: 'https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=dc4f8ae1-13da-43bd-ad13-60fe2c337380',
+    examUrl: 'https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&id=wuhu0UwHzUWb_hhHjC1yyXs2qqn3lLBAo1y3ITkn_01UMVUzTFUzM0ozODNTQzFLQzY1N05IQk1VRC4u',
+    videoCompleted: false,
+    examPassed: false,
+    duration: 0
+  },
+  {
+    id: 3,
+    title: 'Inducción ProScience',
+    description: 'Curso de aminoácidos',
+    videoUrl: 'https://prosciencelab-my.sharepoint.com/personal/coordinador_ti_proscience_com_co/_layouts/15/embed.aspx?UniqueId=dc4f8ae1-13da-43bd-ad13-60fe2c337380',
+    examUrl: 'https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&id=wuhu0UwHzUWb_hhHjC1yyXs2qqn3lLBAo1y3ITkn_01UMVUzTFUzM0ozODNTQzFLQzY1N05IQk1VRC4u',
+    videoCompleted: false,
+    examPassed: false,
+    duration: 0
+  },
 ];
