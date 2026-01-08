6dc574d46cb27a5484549ca7ed168f1a0616341c
LuisVelez <126474251+LuisVelez1@users.noreply.github.com>
2026-01-08 16:38:03 -0500

    Update enviroment.prod.ts

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://intranet-api-gateway.onrender.com',
+  apiGatewayUrl: 'https://api-gateway-v121.onrender.com',
   eurekaUrl: 'https://intranet-eureka-server.onrender.com/eureka'
 };
