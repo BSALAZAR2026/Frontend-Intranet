fcd8b5bb81b1b14fc680466e9a65e7cc303aa4b6
Auxiliar ProScience <auxticproscience@gmail.com>
2026-01-08 16:45:43 -0500

    Update enviroment.prod.ts

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://api-gateway-v121.onrender.com',
+  apiGatewayUrl: 'https://api-gateway-v121.onrender.co',
   eurekaUrl: 'https://intranet-eureka-server.onrender.com/eureka'
 };
