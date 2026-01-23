9663b9140f3526ce78b562ddc52bd7d0f2a4eb08
Aux TI <auxticproscience@gmail.com>
2026-01-23 18:01:55 -0500

    Update deploy routes

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiGatewayUrl: 'http://localhost:8080',
-  eurekaUrl: 'http://localhost:8761/eureka'
+  apiGatewayUrl: 'https://api-gateway-exl9.onrender.com',
+  eurekaUrl: 'https://intranet-eureka-service-ynqu.onrender.com/eureka'
 };
