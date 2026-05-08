0154cfed942e422c37f950843fd1f9f841bc22ba
Aux TI <auxticproscience@gmail.com>
2026-05-08 09:24:37 -0500

    Fixes home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { HomeRightComponent } from "../home-right/home-right";
 
+interface Field{
+  title: string,
+  type: 'image' | 'video',
+  src: string,
+  description?: string;
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -28,4 +35,28 @@ export class HomeComponent {
     this.profileOpen.update(open => !open); 
   }
 
+fields: Field[]= [
+  {
+    title: 'Imagen 1',
+    type: 'image',
+    src: 'assets/images/ImagenHome.jpg',
+    description: ''
+  },
+  {
+    title: 'Imagen 2',
+    type: 'image',
+    src: 'assets/images/ImagenHome2.jpg',
+    description: ''
+  }
+]
+currentIndex = 0;
+
+next() {
+  this.currentIndex = (this.currentIndex + 1) % this.fields.length;
+}
+
+prev() {
+  this.currentIndex =
+    (this.currentIndex - 1 + this.fields.length) % this.fields.length;
+}
 }
